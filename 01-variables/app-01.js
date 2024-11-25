9b32bf4ed16e0d2aea6481afd254a510964bfd95
Gelmetti <gelmettiteam@gmail.com>
2024-11-25 13:50:40 +0100

    java

---
+++ 01-variables/app-01.js
@@ -0,0 +1,41 @@
+//tres formas de declarar variables
+var producto = "monitor de 23 pulgadas";
+
+console.log(producto);
+
+producto= "teclado usb";
+
+console.log(typeof producto);
+
+// tiene tipado dinamico
+
+producto = 12;
+
+console.log(typeof producto);
+
+var disponible = true;
+
+console.log(disponible);
+
+var suma = 34;
+
+console.log("la suma vale:"  + suma);
+
+console.log( suma + 3 + "6");
+
+console.log("6" + (suma + 3));
+
+// iniciar variable seguida....
+
+
+var cosas;
+
+cosas = "cables, placas, alicates";
+
+var categorida = "deportes", marca = "nike", indice = 3;
+
+// let y const
+
+
+
+
